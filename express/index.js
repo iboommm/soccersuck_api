c55729c5beadbd082ab87ceea3149daeb0ed0003
iboommm <iboommm@live.com>
2019-07-19 19:17:26 +0700

    Update get topic

--- express/index.js
+++ express/index.js
@@ -4,97 +4,97 @@ const { parse } = require("node-html-parser");
 const striptags = require("striptags");
 const _ = require("lodash");
 
-
-
 const app = express();
 
 const board = require('./board');
+const topic = require('./topic');
 const port = 80;
 
 app.get("/", (req, res) => res.send("Service is running!"));
 
-app.get("/home", async (req, res) => {
-  let data = await getIndex();
-  let arr = {
-    big_news: queryBigNews(data),
-    big_news_list: queryListBigNews(data),
-    news_today_list: queryListNews(data, 'today'),
-    news_yesterday_list: queryListNews(data, 'yesterday')
-  };
-  res.send({ result: true, data: arr });
+app.get("/home", async(req, res) => {
+    let data = await getIndex();
+    let arr = {
+        big_news: queryBigNews(data),
+        big_news_list: queryListBigNews(data),
+        news_today_list: queryListNews(data, 'today'),
+        news_yesterday_list: queryListNews(data, 'yesterday')
+    };
+    res.send({ result: true, data: arr });
 });
+
 app.use('/', board);
+app.use('/', topic);
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-const getIndex = async () => {
-  try {
-    return await axios.get("http://www.soccersuck.com/");
-  } catch (error) {
-    console.error(error);
-  }
+const getIndex = async() => {
+    try {
+        return await axios.get("http://www.soccersuck.com/");
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const queryBigNews = data => {
-  const root = parse(data.data);
-  let query_html = root.querySelector(".headline_big_desc").toString();
-  let bigNews = _.map(
-    _.filter(_.split(striptags(query_html), "\n"), result => {
-      if (result != "") return result.replace(/\s/g, "");
-    }),
-    data => {
-      return data.replace(/\s/g, "");
-    }
-  );
-  let img = root.querySelector(".headline_big_pic img").attributes;
-  let link = root.querySelector(".headline_main a").attributes;
-  let result = {
-    img: img.src,
-    link: link.href,
-    title: bigNews[0],
-    description: bigNews[1]
-  };
-  return result;
+    const root = parse(data.data);
+    let query_html = root.querySelector(".headline_big_desc").toString();
+    let bigNews = _.map(
+        _.filter(_.split(striptags(query_html), "\n"), result => {
+            if (result != "") return result.replace(/\s/g, "");
+        }),
+        data => {
+            return data.replace(/\s/g, "");
+        }
+    );
+    let img = root.querySelector(".headline_big_pic img").attributes;
+    let link = root.querySelector(".headline_main a").attributes;
+    let result = {
+        img: img.src,
+        link: link.href,
+        title: bigNews[0],
+        description: bigNews[1]
+    };
+    return result;
 };
 
 const queryListBigNews = data => {
-  const root = parse(data.data);
-  let result = _.map(
-    root.querySelectorAll(".headline_main a"),
-    (data, index) => {
-      if (index != 0) {
-        let icon = _.compact(_.map(data.querySelectorAll("img"), img => {
-            if(_.includes(img.attributes.src, 'soccersuck')) {
-                return img.attributes.src;
+    const root = parse(data.data);
+    let result = _.map(
+        root.querySelectorAll(".headline_main a"),
+        (data, index) => {
+            if (index != 0) {
+                let icon = _.compact(_.map(data.querySelectorAll("img"), img => {
+                    if (_.includes(img.attributes.src, 'soccersuck')) {
+                        return img.attributes.src;
+                    }
+                }))[0];
+                return {
+                    link: data.attributes.href,
+                    icon,
+                    title: striptags(data.innerHTML.replace(/\s/g, ""))
+                };
             }
-        }))[0];
-        return {
-          link: data.attributes.href,
-          icon,
-          title: striptags(data.innerHTML.replace(/\s/g, ""))
-        };
-      }
-    }
-  );
-  return _.compact(result);
+        }
+    );
+    return _.compact(result);
 };
 
 const queryListNews = (data, mode) => {
     const root = parse(data.data);
     let result = _.map(
-      root.querySelectorAll(`${(mode == "today" ? ".lastpanel1":".lastpanel2")} .latestnews_tr`),
-      (data, index) => {
-          let title = data.attributes.title;
-          let icon = _.compact(_.map(data.querySelectorAll("img"), img => {
-            if(_.includes(img.attributes.src, 'soccersuck')) {
-                return img.attributes.src;
-            }
-        }))[0];
-        let link = data.querySelector("a").attributes.href
-       return {title,icon,link}
-      }
+        root.querySelectorAll(`${(mode == "today" ? ".lastpanel1":".lastpanel2")} .latestnews_tr`),
+        (data, index) => {
+            let title = data.attributes.title;
+            let icon = _.compact(_.map(data.querySelectorAll("img"), img => {
+                if (_.includes(img.attributes.src, 'soccersuck')) {
+                    return img.attributes.src;
+                }
+            }))[0];
+            let link = data.querySelector("a").attributes.href
+            return { title, icon, link }
+        }
     );
     return _.compact(result);
-  };
-  
+};
---
+++ express/topic.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const { parse } = require("node-html-parser");
+const striptags = require("striptags");
+const _ = require("lodash");
+
+const CircularJSON = require("circular-json");
+
+const topic = require('express').Router();
+
+topic.get("/", (req, res) => res.send("Service is running!"));
+
+topic.get("/topic/:id/:page", async(req, res) => {
+    let id = req.params.id || 1;
+    let page = req.params.page || 1;
+    let data = await getTopic(id, page);
+    let root = parse(data.data);
+    let arr = {
+        topic: queryTopic(root),
+        comment: queryComment(root, page)
+    };
+    res.send({ result: true, data: arr });
+});
+
+const getTopic = async(id, page) => {
+    try {
+        return await axios.get(
+            `http://www.soccersuck.com/boards/topic/${id}/${page}`
+        );
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const queryTopic = root => {
+    let title = root.querySelector(".post_head_topic_news").innerHTML;
+    let description = root.querySelector(".post_desc").removeWhitespace().innerHTML;
+    let datetime = _.split(root.querySelector(".topic_thead_td").innerHTML, "by")[0];
+    let create_by = striptags(_.split(root.querySelector(".topic_thead_td").innerHTML, "by")[1]).slice(1, -1);
+
+    let result = { title, description, datetime, create_by };
+    return result;
+};
+
+const queryComment = (root, page) => {
+    let comment = _.map(root.querySelectorAll('.post_panel'), (cm, key) => {
+        if ((key != 0 && page == 1) || page > 1) {
+            let create_by = cm.querySelector('.user_name a').innerHTML;
+            let create_time = cm.querySelector('.userinfo_time span').innerHTML;
+            let description = cm.querySelector('.post_panel_td_right .post_desc').removeWhitespace().innerHTML;
+
+
+            return { create_by, description, create_time }
+        }
+    });
+    let result = comment;
+    return _.compact(result);
+};
+
+
+
+module.exports = topic;

0338d3f2dab1f144364aa69c62a818f1317a8300
iboommm <iboommm@live.com>
2019-07-21 03:03:42 +0700

    Update field id

--- express/board.js
+++ express/board.js
@@ -7,47 +7,59 @@ const board = require('express').Router();
 
 board.get("/", (req, res) => res.send("Service is running!"));
 
-board.get("/board/:id/:page", async (req, res) => {
-  let id = req.params.id || 1;
-  let page = req.params.page || 1;
-  let data = await getIndex(id,page);
-  let arr = {
-    topic: queryBoard(data)
-  };
-  res.send({ result: true, data: arr });
+board.get("/board/:id", async(req, res) => {
+    let id = req.params.id || 1;
+    let page = 1;
+    let data = await getIndex(id, page);
+    let arr = {
+        topic: queryBoard(data)
+    };
+    res.send({ result: true, data: arr });
 });
 
-const getIndex = async (id,page) => {
-  try {
-    return await axios.post(
-      "http://www.soccersuck.com/boards/loadTopicPage/",
-      `id=${id}&page=${page}&limit=60`
-    );
-  } catch (error) {
-    console.error(error);
-  }
+board.get("/board/:id/:page", async(req, res) => {
+    let id = req.params.id || 1;
+    let page = req.params.page || 1;
+    let data = await getIndex(id, page);
+    let arr = {
+        topic: queryBoard(data)
+    };
+    res.send({ result: true, data: arr });
+});
+
+const getIndex = async(id, page) => {
+    try {
+        return await axios.post(
+            "http://www.soccersuck.com/boards/loadTopicPage/",
+            `id=${id}&page=${page}&limit=60`
+        );
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const queryBoard = data => {
-  const root = parse(data.data);
-  let result = _.map(
-    root.querySelectorAll(".board_topic_row"),
-    (data, index) => {
-      let icon = data.querySelector(".board_topic_col_icon img").attributes.src;
-      let link = data.querySelector(".board_topic_col_topic a").attributes.href;
-      let title = data.querySelector(".board_topic_col_topic a").innerHTML;
-      let col_data = _.compact(
-        _.map(data.querySelectorAll(".board_topic_col"), (col, key) => {
-          if (key > 0 && key < 4) {
-            return striptags(col.querySelector("p").innerHTML);
-          }
-        })
-      );
-      let [comment, read, create_by] = col_data;
-      return { icon, link, title, comment, read, create_by };
-    }
-  );
-  return _.compact(result);
+    const root = parse(data.data);
+    let result = _.map(
+        root.querySelectorAll(".board_topic_row"),
+        (data, index) => {
+            let icon = data.querySelector(".board_topic_col_icon img").attributes.src;
+            let link = data.querySelector(".board_topic_col_topic a").attributes.href;
+            let id = _.last(_.split(link, "/"));
+            let pin = data.querySelectorAll(".board_topic_col_icon img").length == 2 ? true : false;
+            let title = data.querySelector(".board_topic_col_topic a").innerHTML;
+            let col_data = _.compact(
+                _.map(data.querySelectorAll(".board_topic_col"), (col, key) => {
+                    if (key > 0 && key < 4) {
+                        return striptags(col.querySelector("p").innerHTML);
+                    }
+                })
+            );
+            let [comment, read, create_by] = col_data;
+            return { id, icon, link, title, comment, read, create_by, pin };
+        }
+    );
+    return _.compact(result);
 };
 
 module.exports = board;
--- express/index.js
+++ express/index.js
@@ -11,22 +11,22 @@ const topic = require("./topic");
 const port = 8888;
 
 app.all("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
 });
 
 app.get("/", (req, res) => res.send("Service is running!"));
 
-app.get("/home", async (req, res) => {
-  let data = await getIndex();
-  let arr = {
-    big_news: queryBigNews(data),
-    big_news_list: queryListBigNews(data),
-    news_today_list: queryListNews(data, "today"),
-    news_yesterday_list: queryListNews(data, "yesterday")
-  };
-  res.send({ result: true, data: arr });
+app.get("/home", async(req, res) => {
+    let data = await getIndex();
+    let arr = {
+        big_news: queryBigNews(data),
+        big_news_list: queryListBigNews(data),
+        news_today_list: queryListNews(data, "today"),
+        news_yesterday_list: queryListNews(data, "yesterday")
+    };
+    res.send({ result: true, data: arr });
 });
 
 app.use("/", board);
@@ -34,78 +34,80 @@ app.use("/", topic);
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-const getIndex = async () => {
-  try {
-    return await axios.get("http://www.soccersuck.com/");
-  } catch (error) {
-    console.error(error);
-  }
+const getIndex = async() => {
+    try {
+        return await axios.get("http://www.soccersuck.com/");
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const queryBigNews = data => {
-  const root = parse(data.data);
-  let query_html = root.querySelector(".headline_big_desc").toString();
-  let bigNews = _.map(
-    _.filter(_.split(striptags(query_html), "\n"), result => {
-      if (result != "") return result.replace(/\s/g, "");
-    }),
-    data => {
-      return data.replace(/\s/g, "");
-    }
-  );
-  let img = root.querySelector(".headline_big_pic img").attributes;
-  let link = root.querySelector(".headline_main a").attributes;
-  let result = {
-    img: img.src,
-    link: link.href,
-    title: bigNews[0],
-    description: bigNews[1]
-  };
-  return result;
+    const root = parse(data.data);
+    let query_html = root.querySelector(".headline_big_desc").toString();
+    let bigNews = _.map(
+        _.filter(_.split(striptags(query_html), "\n"), result => {
+            if (result != "") return result.replace(/\s/g, "");
+        }),
+        data => {
+            return data.replace(/\s/g, "");
+        }
+    );
+    let img = root.querySelector(".headline_big_pic img").attributes;
+    let link = root.querySelector(".headline_main a").attributes;
+    let result = {
+        img: img.src,
+        link: link.href,
+        id: (_.last(_.split(link.href, "/"))),
+        title: bigNews[0],
+        description: bigNews[1]
+    };
+    return result;
 };
 
 const queryListBigNews = data => {
-  const root = parse(data.data);
-  let result = _.map(
-    root.querySelectorAll(".headline_main a"),
-    (data, index) => {
-      if (index != 0) {
-        let icon = _.compact(
-          _.map(data.querySelectorAll("img"), img => {
-            if (_.includes(img.attributes.src, "soccersuck")) {
-              return img.attributes.src;
+    const root = parse(data.data);
+    let result = _.map(
+        root.querySelectorAll(".headline_main a"),
+        (data, index) => {
+            if (index != 0) {
+                let icon = _.compact(
+                    _.map(data.querySelectorAll("img"), img => {
+                        if (_.includes(img.attributes.src, "soccersuck")) {
+                            return img.attributes.src;
+                        }
+                    })
+                )[0];
+                return {
+                    link: data.attributes.href,
+                    id: (_.last(_.split(data.attributes.href, "/"))),
+                    icon,
+                    title: striptags(data.innerHTML.replace(/\s/g, ""))
+                };
             }
-          })
-        )[0];
-        return {
-          link: data.attributes.href,
-          icon,
-          title: striptags(data.innerHTML.replace(/\s/g, ""))
-        };
-      }
-    }
-  );
-  return _.compact(result);
+        }
+    );
+    return _.compact(result);
 };
 
 const queryListNews = (data, mode) => {
-  const root = parse(data.data);
-  let result = _.map(
-    root.querySelectorAll(
-      `${mode == "today" ? ".lastpanel1" : ".lastpanel2"} .latestnews_tr`
-    ),
-    (data, index) => {
-      let title = data.attributes.title;
-      let icon = _.compact(
-        _.map(data.querySelectorAll("img"), img => {
-          if (_.includes(img.attributes.src, "soccersuck")) {
-            return img.attributes.src;
-          }
-        })
-      )[0];
-      let link = data.querySelector("a").attributes.href;
-      return { title, icon, link };
-    }
-  );
-  return _.compact(result);
+    const root = parse(data.data);
+    let result = _.map(
+        root.querySelectorAll(
+            `${mode == "today" ? ".lastpanel1" : ".lastpanel2"} .latestnews_tr`
+        ),
+        (data, index) => {
+            let title = data.attributes.title;
+            let icon = _.compact(
+                _.map(data.querySelectorAll("img"), img => {
+                    if (_.includes(img.attributes.src, "soccersuck")) {
+                        return img.attributes.src;
+                    }
+                })
+            )[0];
+            let link = data.querySelector("a").attributes.href;
+            return { title, icon, link, id: (_.last(_.split(link, "/"))), };
+        }
+    );
+    return _.compact(result);
 };
